e8d7302caa72e85673d5efa0072e3c37b81b4601
Mesut-Gok <mesutcukgok@gmail.com>
2026-02-16 22:03:04 +0200

    Add project files.

---
+++ Lab 2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(7, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(785, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(463, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 50);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "A";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(268, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "A";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(244, 418);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(318, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Letter Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab 2/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Form1 : Form
+    {
+        const int N = 3; 
+        Label[] labels = new Label[N];
+
+        Random rand = new Random();
+
+        int points = 0;
+
+        int speedcheck = 0;
+
+        bool GameOver = false;  
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+            label4.Text = " Points : " + points;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labels[0] = label1;
+            labels[1] = label2;
+            labels[2] = label3;
+            
+                        for (int i = 0; i < N; i++)
+                        {
+                            labels[i].Top = 0;
+                            labels[i].Left = rand.Next(0, panel1.Width - labels[i].Width);
+                            labels[i].Text = Convert.ToString(Convert.ToChar(rand.Next(65, 91)));
+
+
+                        }
+
+            label4.Text = " Points : " + points;
+        }
+
+        // This event triggers every tick to move labels down and check for boundaries
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+            for (int i = 0; i < N; i++)
+            {
+                labels[i].Top += 5;
+
+                if (labels[i].Top > panel1.Height - labels[i].Height)
+                {
+                    labels[i].Top = 0;
+                    labels[i].Left = rand.Next(0, panel1.Width - labels[i].Width);
+                    labels[i].Text = Convert.ToString(Convert.ToChar(rand.Next(65, 91)));
+
+                    points = points - 5;
+                    label4.Text = " Points : " + points;
+                    
+                }
+
+                
+            }
+
+            
+        }
+        // Handles user input and compares pressed key with active labels
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool correctKey = false;
+
+
+            for (int i = 0; i < N; i++)
+            {
+                if (e.KeyChar == Convert.ToChar(labels[i].Text))
+                {
+                    correctKey = true;
+
+                    labels[i].Top = 0;
+                    labels[i].Left = rand.Next(0, panel1.Width - labels[i].Width);
+                    labels[i].Text = Convert.ToString(Convert.ToChar(rand.Next(65, 91)));
+
+                }
+
+            }
+            if (correctKey)
+            {
+                points = points + 1;
+
+            }
+            else
+            {
+                points = points - 1;
+            }
+            // Increases game speed every time the player gains 5 points
+            if (points >= 5 && (points / 5 > speedcheck))
+            {
+                ++speedcheck;
+                timer1.Interval = timer1.Interval - 100;
+            }
+
+            label4.Text = " Points : " + points;
+
+            if (points < 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Game Over!");
+                GameOver = true;
+                label4.Text = "Game Over!";
+                this.Close();
+            }
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
